ba5a2d0fbd5a385fc41ac75e2064d153cdbde59f
Lisa Poncet <poncet.lisa@gmail.com>
2020-11-13 17:15:28 +0100

    add 2 button sizes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import Button from "./components/Button"
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
+      <h2>Reusable button component</h2>
+      <br/>
       <Button />
       <Button variant="outline" />
       <Button variant="text" />
+      <br/>
       <Button disableShadow />
       <Button disabled />
+      <br/>
+      <Button size="sm" />
+      <Button />
+      <Button size="lg" />
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,9 @@ const Button = (props) => {
   const variant = props.variant ? props.variant : "default";
   const shadow = props.disableShadow ? "no-shadow" : "";
   const disabled = props.disabled ? "disabled" : "";
-  const buttonStyle = `button ${variant} ${shadow} ${disabled}`;
+  const size = props.size ? props.size : "";
+  
+  const buttonStyle = `button ${variant} ${shadow} ${disabled} ${size}`;
 
   return <button className={buttonStyle}>Default</button>;
 };
